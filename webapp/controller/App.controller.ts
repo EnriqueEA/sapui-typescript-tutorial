bbcd70f471edbe7c6f429a12c9ae0d781267576e
EnriqueEA <elescanoea@gmail.com>
2024-12-12 11:40:56 -0500

    step 9 completed: Component Configuration

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,36 +1,19 @@
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @namespace ui5.walkthrough.controller
  */
 export default class App extends Controller {
-   onInit(): void | undefined {
-      // set data model on view
-      const oData = {
-         recipient: {
-            name: "World"
-         }
-      };
-      const oDataModel = new JSONModel(oData);
-      this.getView()?.setModel(oDataModel);
-
-      // set i18n model on view
-      const i18nModel = new ResourceModel({
-         bundleName: "ui5.walkthrough.i18n.i18n"
-      });
-      this.getView()?.setModel(i18nModel, "i18n");
-   }
-
    onSayHelloButtonPress(): void {
       // read msg from i18n model
-      const recipient = (this.getView()?.getModel() as JSONModel).getProperty("/recipient/name");
-      const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel).getResourceBundle() as ResourceBundle;
-      // show message
+      const recipient = (<JSONModel>this.getView()?.getModel())?.getProperty("/recipient/name");
+      const resourceBundle = <ResourceBundle>(<ResourceModel>this.getView()?.getModel("i18n"))?.getResourceBundle();
       const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
+      // show message
       MessageToast.show(msg);
    }
 };
