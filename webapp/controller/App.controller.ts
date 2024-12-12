b5ee2f6991d860e855f0c8661ac35a057c109cc8
EnriqueEA <elescanoea@gmail.com>
2024-12-12 10:26:02 -0500

    step 8 completed: Translatable Texts

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,8 @@
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @namespace ui5.walkthrough.controller
@@ -15,11 +17,20 @@ export default class App extends Controller {
       };
       const oDataModel = new JSONModel(oData);
       this.getView()?.setModel(oDataModel);
+
+      // set i18n model on view
+      const i18nModel = new ResourceModel({
+         bundleName: "ui5.walkthrough.i18n.i18n"
+      });
+      this.getView()?.setModel(i18nModel, "i18n");
    }
 
    onSayHelloButtonPress(): void {
-      const oModel = this.getView()?.getModel() as JSONModel;
-      const sRecipientName = oModel.getProperty("/recipient/name");
-      MessageToast.show(`Hello ${sRecipientName}`);
+      // read msg from i18n model
+      const recipient = (this.getView()?.getModel() as JSONModel).getProperty("/recipient/name");
+      const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel).getResourceBundle() as ResourceBundle;
+      // show message
+      const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
+      MessageToast.show(msg);
    }
 };
